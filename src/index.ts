d0398ecfbb568221280a7d009f0025883d93d11c
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2024-09-18 10:09:03 +0100

    custom url

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import express, {Application, Request, Response} from "express" ;
 import morgan from "morgan";
+import {stringify} from "querystring";
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express();
 
 app.use(morgan("tiny"));
-app.get("/ping", async (_req : Request, res: Response) => {
+app.get("/ping/:name", async (req : Request, res: Response) => {
     res.send({
-        message: "hello from Una",
+        message: "Hello " + req.params.name,
     });
 });
 
-app.get('/bananas', async (_req : Request, res: Response)=> {
+app.get('/bananas', async (req : Request, res: Response)=> {
     res.send('hello world, this is bananas');
 });
 
